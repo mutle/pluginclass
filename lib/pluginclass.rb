d46f7acad192cc9c7c11b3cd7a948c1e590bc874
Mutwin Kraus <mutwin.kraus@gmail.com>
2010-02-22 22:56:21 +0100

    Initial PluginClass implementation

---
+++ lib/pluginclass.rb
@@ -0,0 +1,70 @@
+module PluginClass
+
+  module ObjectExtensions
+    def is_a?(klass)
+      if klass.respond_to?(:root_klass) && klass.respond_to?(:current_klass)
+        return super(klass.root_klass) || super(klass.current_klass)
+      end
+      super(klass)
+    end
+
+    def instance_of?(klass)
+      if klass.respond_to?(:root_klass) && klass.respond_to?(:current_klass)
+        return super(klass.root_klass) || super(klass.current_klass)
+      end
+      super(klass)
+    end
+  end
+
+  class Proxy
+    attr_reader :root_klass, :current_klass
+
+    def initialize(klass)
+      @root_klass = @current_klass = klass
+      @extended = false
+    end
+
+    def extend_class(&block)
+      unless @extended
+        @current_klass = Class.new(@root_klass)
+        @extended = true
+      end
+      @current_klass.class_eval &block
+    end
+
+    def reset_class
+      @current_klass = @root_klass
+      @extended = false
+    end
+
+    def method_missing(meth, *args)
+      @current_klass.send(meth, *args)
+    end
+
+  end
+
+
+  class << self
+    def extend_class(name, &block)
+      if klass = plugin_classes[name]
+        klass.extend_class &block
+      end
+    end
+
+    def plugin_classes
+      @plugin_classes ||= {}
+    end
+
+    def included(mod)
+      super(mod)
+      proxy = Proxy.new(mod)
+      name = mod.name
+      plugin_classes[name] = proxy
+      Object.send(:remove_const, name)
+      Object.const_set(name, proxy)
+    end
+  end
+
+end
+
+Object.send(:include, PluginClass::ObjectExtensions)
--- spec/pluginclass_spec.rb
+++ spec/pluginclass_spec.rb
@@ -1,7 +1,66 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Pluginclass" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "PluginClass" do
+
+  it "adds and removes features from class" do
+    class TestClass
+      include PluginClass
+    end
+    TestClass.new.should_not respond_to(:foo)
+
+    PluginClass.extend_class "TestClass" do
+      def foo
+        "foo"
+      end
+    end
+    TestClass.new.should respond_to(:foo)
+
+    TestClass.reset_class
+    TestClass.new.should_not respond_to(:foo)
   end
+
+  it "supports instance_of" do
+    class TestClass2
+      include PluginClass
+    end
+    true.should_not be_instance_of(TestClass2)
+    [].should_not be_instance_of(TestClass2)
+    {}.should_not be_instance_of(TestClass2)
+
+    TestClass2.new.should be_instance_of(TestClass2)
+    PluginClass.extend_class "TestClass2" do
+    end
+    TestClass2.new.should be_instance_of(TestClass2)
+    TestClass2.reset_class
+    TestClass2.new.should be_instance_of(TestClass2)
+  end
+
+  it "supports is_a" do
+    class TestClass3
+      include PluginClass
+    end
+    true.should_not be_is_a(TestClass3)
+    [].should_not be_is_a(TestClass3)
+    {}.should_not be_is_a(TestClass3)
+
+    TestClass3.new.should be_is_a(TestClass3)
+    PluginClass.extend_class "TestClass3" do
+    end
+    TestClass3.new.should be_is_a(TestClass3)
+    TestClass3.reset_class
+    TestClass3.new.should be_is_a(TestClass3)
+  end
+
+  it "returns the root klasses name" do
+    class TestClass4
+      include PluginClass
+    end
+    TestClass4.new.class.name.should == "TestClass4"
+    PluginClass.extend_class "PluginClass4" do
+    end
+    TestClass4.new.class.name.should == "TestClass4"
+    TestClass4.reset_class
+    TestClass4.new.class.name.should == "TestClass4"
+  end
+
 end
